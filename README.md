0383098575fde5389d9952ff420d71ff99807103
MoonWanki <dhksrl2589@gmail.com>
2019-02-24 23:57:30 +0900

    Add carousel, firebase

--- README.md
+++ README.md
@@ -5,44 +5,46 @@
 [![MaterialUI](https://img.shields.io/badge/Material--UI-v3.9.2-ee59a4.svg)](https://material-ui.com/)
 
 Official website | https://www.octopusfantasy.com  
-Personal platform by MoonWanki. Personal blog as primary contents.  
+Personal platform with UCC works and online game services by Moon Wanki.  
   
 ## Products
 
-### **The Great Venus** - Turn-based Web RPG based on Ethereum platform  
-Official website | https://tgv.octopusfantasy.com  
-GitHub | https://github.com/MoonWanki/thegreatvenus
+### Online game services
 
-### **Octo Mahjong Online**
+- **The Great Venus** - Turn-based Web RPG based on Ethereum platform  
+Official website | https://tgv.octopusfantasy.com  
+GitHub | https://github.com/MoonWanki/the-great-venus
+
+- **Octo Mahjong Online**  
 Official website | https://mahjong.octopusfantasy.com  
 GitHub | https://github.com/MoonWanki/octopusfantasy-mahjong  
   
-## UCC categories
+### UCC works
 
 - **Music Works**  
-Self-made musics and MVs  
+Self-made musics and MVs.  
 
 - **Entertainments**  
-Various funny videos since 2009  
+Various video works produced since 2009.  
 
 - **Daigasso! DX**  
-Music copies with Nintendo DS software 「Daigasso! Band Brothers DX」  
+Music copies with Nintendo DS software 「Daigasso! Band Brothers DX」.  
 - **Game Videos**  
-Video works with gameplay  
+Video works with gameplay.  
 
 ## Dependencies
 
 - **ReactJS** | https://reactjs.org  
-Top 1 Front-end library.  
-
-- **Redux** | https://redux.js.org  
-For state management in React app.  
+Top 1 Front-End library.  
 
 - **Material-UI** | https://material-ui.com  
 Top 1 UI library for React app.  
 
-- **Axios** | https://github.com/axios/axios  
-Promise based HTTP client.  
+- **Redux** | https://redux.js.org  
+For state management in React app.  
 
 - **Redux Promise Middleware** | https://github.com/pburtchaell/redux-promise-middleware  
-Enables simple, yet robust handling of async action creators in Redux.  
+Simple and robust async action creators in Redux.  
+
+- **Axios** | https://github.com/axios/axios  
+Promise based HTTP client.  
--- src/Components/Carousel/index.js
+++ src/Components/Carousel/index.js
@@ -1,10 +1,88 @@
 import React, { Component } from 'react'
+import './index.scss'
+
+import { IconButton, Icon } from '@material-ui/core'
+
+import carouselImage1 from 'images/carousel_1.jpg'
+import carouselImage2 from 'images/carousel_2.jpg'
+import carouselImage3 from 'images/carousel_3.jpg'
+
+const duration = 15
+
+const carouselItems = [
+    <div className='carousel-item' style={{ backgroundImage: `url(${carouselImage1})`}}>
+        <p className='carousel-item-text carousel-item-text-title'>새로운 PC. 새로운 시작.</p>
+        <p className='carousel-item-text'>유로트럭 풀옵을 돌리기 위한 그의 부단한 노력.</p>
+    </div>,
+    <div className='carousel-item' style={{ backgroundImage: `url(${carouselImage2})`}}>
+        <p className='carousel-item-text carousel-item-text-title'>文, "경영대학원 사퇴 결정"</p>
+        <p className='carousel-item-text'>4개월 간의 숨가쁜 大장정···끝내 막을 내리다</p>
+    </div>,
+    <div className='carousel-item' style={{ backgroundImage: `url(${carouselImage3})`}}></div>,
+]
+
+let slidingTimer = null
 
 export default class Carousel extends Component {
+
+    state = {
+        current: 0,
+        progress: 0,
+    }
+
+    componentDidMount() {
+        slidingTimer = setTimeout(this.runProgress, duration)
+    }
+
+    componentWillUnmount() {
+        clearTimeout(slidingTimer)
+    }
+
+    runProgress = () => {
+        if(this.state.progress < 100) {
+            this.setState({ progress: this.state.progress + 0.4 }, () => {slidingTimer = setTimeout(this.runProgress, duration)})
+        }
+        else {
+            this.toNext()
+        }
+    }
+
+    toPrev = () => this.toNth(this.state.current===0 ? carouselItems.length - 1 : this.state.current - 1)
+
+    toNext = () => this.toNth(this.state.current < carouselItems.length - 1 ? this.state.current + 1 : 0)
+    
+    toNth = n => {
+        clearTimeout(slidingTimer)
+        this.setState({ current: n, progress: 0 })
+        slidingTimer = setTimeout(this.runProgress, duration)
+    }
+
     render() {
         return (
-            <div style={{ width: '100vw', height: 300, background: 'gray' }}>
-                
+            <div className='carousel'>
+
+                {carouselItems.map((carouselItem, i) => 
+                    <span key={i} className={this.state.current===i ? 'carousel-active' : 'carousel-inactive'}>
+                        {carouselItem}
+                    </span>
+                )}
+
+                <div className='carousel-arrows'>
+                    <IconButton onClick={this.toPrev}><Icon style={{ fontSize: 30, color: 'white'}}>navigate_before</Icon></IconButton>
+                    <IconButton onClick={this.toNext}><Icon style={{ fontSize: 30, color: 'white'}}>navigate_next</Icon></IconButton>
+                </div>
+
+                <div className='carousel-navigator'>
+                    {carouselItems.map((_, i) =>
+                        <div
+                            key={i}
+                            onClick={() => { if(this.state.current!==i) this.toNth(i) }}
+                            className='carousel-navigator-button'
+                            style={this.state.current===i ? { opacity: 0.8 } : { cursor: 'pointer' }} />
+                    )}
+                </div>
+
+                <div className='carousel-progress-bar' style={{ width: `${this.state.progress > 100 ? 100 : this.state.progress}%` }} />
             </div>
         )
     }
