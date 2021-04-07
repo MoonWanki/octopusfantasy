8f584ea7edb309b5c72ae001b4c79dba75e84d48
MoonWanki <dhksrl2589@gmail.com>
2021-04-07 21:01:14 +0900

    Update coworker history

--- src/Layout/About.js
+++ src/Layout/About.js
@@ -46,13 +46,13 @@ class About extends Component {
                             <div className='about-crews-profile-content'>
                                 <div className='about-crews-profile-header'><span style={{ fontSize: '1.4em' }}>Moon Wanki <span style={{ color: 'lightgray' }}>|</span></span> Owner</div>
                                 <div className='about-crews-profile-text'>
-                                    1994.4&emsp;출생<br />
-                                    2013.3&emsp;아주대학교 미디어학과 입학<br />
-                                    2014.6 ~ 2016.6&emsp;공군 제10전투비행단 서버실 근무<br />
-                                    2018.6 ~ 2018.8&emsp;㈜ Quram 근무<br />
-                                    2018.12 ~ 2019.2&emsp;㈜ Kidsoft 근무<br />
-                                    2019.5 ~&emsp;SK주식회사 C&amp;C 근무<br />
-                                    2019.8 ~&emsp;삼성전자 무선사업부 근무<br />
+                                    1994.04 ~ &emsp;출생<br />
+                                    2013.03 ~ &emsp;아주대학교 미디어콘텐츠전공 학사 과정<br />
+                                    2014.06 ~ 2016.06&emsp;공군 제10전투비행단 서버실 근무<br />
+                                    2018.06 ~ 2018.08&emsp;Quram 근무<br />
+                                    2018.12 ~ 2019.02&emsp;Kidsoft 근무<br />
+                                    2019.05 ~ 2019.05&emsp;SK주식회사 C&amp;C 근무<br />
+                                    2019.08 ~ &emsp;삼성전자 무선사업부 근무<br />
                                 </div>
                             </div>
                         </div>
@@ -64,10 +64,10 @@ class About extends Component {
                             <div className='about-crews-profile-content'>
                                 <div className='about-crews-profile-header'><span style={{ fontSize: '1.4em' }}>Jamlee <span style={{ color: 'lightgray' }}>|</span></span> Coworker</div>
                                 <div className='about-crews-profile-text'>
-                                    1993.4&emsp;출생<br />
-                                    2013.3&emsp;아주대학교 미디어학과 입학<br />
-                                    2014.10 ~ 2016.7 &emsp;대한민국 청와대 대통령실 경호처 제1경비단 근무<br />
-                                    2019.3 ~&emsp;LG CNS 근무<br />
+                                    1993.04 ~ &emsp;출생<br />
+                                    2013.03 ~ &emsp;아주대학교 미디어콘텐츠전공 학사 과정<br />
+                                    2014.10 ~ 2016.07 &emsp;대한민국 청와대 대통령실 경호처 제1경비단 근무<br />
+                                    2019.03 ~ &emsp;LG CNS 근무<br />
                                 </div>
                             </div>
                         </div>
@@ -79,11 +79,12 @@ class About extends Component {
                             <div className='about-crews-profile-content'>
                                 <div className='about-crews-profile-header'><span style={{ fontSize: '1.4em' }}>Junseo Park <span style={{ color: 'lightgray' }}>|</span></span> Coworker</div>
                                 <div className='about-crews-profile-text'>
-                                    1994.3&emsp;출생<br />
-                                    2013.3&emsp;아주대학교 미디어학과 입학<br />
-                                    2015.3 ~ 2016.12 &emsp;대한민국 육군 제 12 보병사단 근무<br />
-                                    2019.4 ~ 2019.7 &emsp;NAVER Search&amp;Clova(Data Engineer) 근무<br />
-                                    2019.7 ~&emsp;매그니스 근무<br />
+                                    1994.03 ~ &emsp;출생<br />
+                                    2013.03 ~ &emsp;아주대학교 미디어콘텐츠전공 학사 과정<br />
+                                    2015.03 ~ 2016.12 &emsp;대한민국 육군 제 12 보병사단 근무<br />
+                                    2019.04 ~ 2019.07 &emsp;NAVER Search&amp;Clova(Data Engineer) 근무<br />
+                                    2019.07 ~ 2019.09 &emsp;매그니스 근무<br />
+                                    2020.04 ~ &emsp;신세계 I&amp;C 근무<br />
                                 </div>
                             </div>
                         </div>
