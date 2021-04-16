6317029274d19f97ea8b47a69516c7bf7ad5760c
MoonWanki <dhksrl2589@gmail.com>
2021-04-16 20:27:32 +0900

    Remove component: Carousel

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,7 +1,6 @@
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as ProfileZone } from './ProfileZone';
-export { default as Carousel } from './Carousel';
 export { default as PostView } from './PostView';
 export { default as Loading } from './Loading';
 export { default as VideoBanner } from './VideoBanner';
