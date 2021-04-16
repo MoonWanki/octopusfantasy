e351a614881d65f124d6ab87103269e1f39cae19
MoonWanki <dhksrl2589@gmail.com>
2021-04-16 17:10:22 +0900

    Add onLoaded props on VideoBanner

--- src/Components/VideoBanner/index.tsx
+++ src/Components/VideoBanner/index.tsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import Player from 'react-player/lazy';
 import video from '~/assets/videos/home_banner_video.mp4';
 import { Fade } from 'react-awesome-reveal';
 import './index.scss';
 
-export default function VideoBanner()
+interface Props
 {
+    onLoaded?: () => void,
+}
+
+export default function VideoBanner({ onLoaded }: Props)
+{
+    const [ isVideoBufferEnd, setVideoBufferEnd ] = useState<Boolean>(false);
+
+    const onVideoBufferEnd = useCallback(() => {
+        setVideoBufferEnd(true);
+        if(onLoaded != undefined)
+        {
+            onLoaded();
+        }
+    }, []);
+
     return (
         <div className='video-banner-wrapper'>
-            <Player
-                url={video}
-                playing={true}
-                loop={true}
-                muted={true}
-                width='100%'
-                height=''
-            />
-            <div className='video-banner-inner'>
-                <div className='video-banner-inner-image'></div>
-                <Fade triggerOnce direction='up' delay={400}>
-                    <a target='_blank' rel='noopener noreferrer' href='https://youtu.be/U1wU3euSLd0'>
-                        <p className='video-banner-inner-button'>Watch on <span style={{ fontWeight: 500 }}>YouTube</span></p>
-                    </a>
-                </Fade>
-            </div>
+            <Fade triggerOnce>
+                <Player
+                    url={video}
+                    playing={true}
+                    loop={true}
+                    muted={true}
+                    width='100%'
+                    height=''
+                    onBufferEnd={onVideoBufferEnd}
+                />
+            </Fade>
+            {isVideoBufferEnd &&
+                <div className='video-banner-inner'>
+                    <div className='video-banner-inner-image'></div>
+                    <Fade triggerOnce direction='up' delay={400}>
+                        <a target='_blank' rel='noopener noreferrer' href='https://youtu.be/U1wU3euSLd0'>
+                            <p className='video-banner-inner-button'>Watch on <span style={{ fontWeight: 500 }}>YouTube</span></p>
+                        </a>
+                    </Fade>
+                </div>
+            }
         </div>
     );
 }
