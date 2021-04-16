87cfc95bfedf0a0fd4a6a91ab261d03c85dd89a7
MoonWanki <dhksrl2589@gmail.com>
2021-04-17 01:44:41 +0900

    Renewal pages: Home, About

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -15,28 +15,7 @@ export default function About(): ReactElement {
     return (
         <Fragment>
             <Header />
-            <div className='about-banner'>
-                <div className='about-banner-inner'>
-                    <div>
-                        <span style={{ fontWeight: 500 }}>Octopus Fantasy</span>의 <span style={{ fontWeight: 500 }}>Owner</span>인 <span style={{ fontWeight: 500 }}>Moon Wanki</span>는<br />
-                        중학교 3학년이었던 2009년부터 <span style={{ fontWeight: 500 }}>'Octopus Fantasy'라는 이름의 네이버 블로그</span>를 운영하며<br />
-                        왕성한 UCC 활동을 바탕으로 많은 블로거들과의 커뮤니케이션을 즐겨왔습니다.<br />
-                        <br />
-                        <br />
-                    </div>
-                    <div>
-                        오랜 꿈이었던 개인 플랫폼 운영을 성취하고자<br />
-                        <span style={{ fontWeight: 500 }}>현대에 가장 트렌디한 개발 기술을 습득 · 접목</span>하여 <span style={{ fontWeight: 500 }}>직접 제작 및 운영</span>하게 된 플랫폼이 바로<br />
-                        <span style={{ fontWeight: 500 }}>Octopus Fantasy</span> 입니다.<br />
-                        <br />
-                        <br />
-                    </div>
-                    <div>
-                        단순히 수많은 UCC들이 담겨 있는 개인 블로그의 모습에서 나아가,<br />
-                        <span style={{ fontWeight: 500 }}><span style={{ color: 'gold' }}>신기술을 기반으로 한 다양하고 새로운 컨텐츠를 서비스하는 플랫폼</span>으로 발돋움하고자 합니다.</span><br />
-                    </div>
-                </div>
-            </div>
+            <div className='about-banner' />
             <div className='about-crews'>
                 <div className='about-crews-profile'>
                     <div className='about-crews-profile-avatar' style={{ backgroundImage: `url(${crewImageOwner})` }} />
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,33 +1,95 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useCallback, Fragment } from 'react';
 import { Header, Footer, VideoBanner } from '~/components';
 import { Link } from 'react-router-dom';
+import { Fade } from 'react-awesome-reveal';
+import quickLinkImageMusic from '~/assets/images/post_banner_music_small.jpg';
+import quickLinkImageEntertainment from '~/assets/images/post_banner_entertainment_small.jpg';
+import quickLinkImageDaigasso from '~/assets/images/post_banner_daigasso_small.jpg';
+import quickLinkImageGamevideo from '~/assets/images/post_banner_gamevideo_small.jpg';
+
 import './Home.scss';
 
-export default function Home() {
+export default function Home()
+{
+    const [ isVideoBannerLoaded, setVideoBannerLoaded ] = useState<boolean>(false);
+
+    const onVideoBannerLoaded = useCallback(() => {
+        setVideoBannerLoaded(true);
+    }, [])
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
 
     return (
-        <div>
+        <div style={{ overflow: 'hidden' }}>
             <Header />
-            <VideoBanner />
-            <div className='home-quick-link'>
-                <Link to='/music' className='home-quick-link-item' style={{ background: '#473d80'}}>
-                    <span className='home-quick-link-item-text'>MUSIC WORKS</span>
-                </Link>
-                <Link to='/entertainment' className='home-quick-link-item' style={{ background: '#c16a70'}}>
-                    <span className='home-quick-link-item-text'>ENTERTAINMENTS</span>
-                </Link>
-                <Link to='/daigasso' className='home-quick-link-item' style={{ background: '#d69739'}}>
-                <   span className='home-quick-link-item-text'>DAIGASSO! DX</span>
-                </Link>
-                <Link to='/gamevideo' className='home-quick-link-item' style={{ background: '#8f3f39'}}>
-                    <span className='home-quick-link-item-text'>GAME VIDEOS</span>
-                </Link>
-            </div>
-            <Footer />
+            <VideoBanner onLoaded={onVideoBannerLoaded} />
+            {isVideoBannerLoaded &&
+                <Fragment>
+                    <div className='home-enjoy-zone'>
+                        <Fade triggerOnce direction='up' delay={500} cascade>
+                            <div className='home-enjoy-zone-title'>
+                                Wanna <span style={{ fontWeight: 500 }}>Enjoy</span>?
+                            </div>
+                        </Fade>
+                        <div className='home-enjoy-zone-link-container'>
+                            <Fade triggerOnce direction='left' delay={700}>
+                                <a href={'https://tgv.octopusfantasy.com'} className='home-enjoy-zone-link-button home-enjoy-zone-link-button-left'>
+                                    <div className='home-enjoy-zone-link-button-text'>
+                                        <div className='home-enjoy-zone-link-button-text-title'>
+                                            The Great Venus
+                                        </div>
+                                        <div className='home-enjoy-zone-link-button-text-description'>
+                                            Turn-based RPG based with Ethereum
+                                        </div>
+                                    </div>
+                                </a>
+                            </Fade>
+                            <Fade triggerOnce direction='right' delay={700}>
+                                <a href={'https://mahjong.octopusfantasy.com'} className='home-enjoy-zone-link-button home-enjoy-zone-link-button-right'>
+                                    <div className='home-enjoy-zone-link-button-text' style={{ alignItems: 'flex-end' }}>
+                                        <div className='home-enjoy-zone-link-button-text-title'>
+                                            Octo Mahjong Online
+                                        </div>
+                                        <div className='home-enjoy-zone-link-button-text-description'>
+                                            Web based Riichi Mahjong
+                                        </div>
+                                    </div>
+                                </a>
+                            </Fade>
+                        </div>
+                    </div>
+                    <div className='home-quick-link-area'>
+                        <div className='home-quick-link-inner'>
+                            <div className='home-quick-link-description'>Everything<br/><span style={{ fontWeight: 500 }}>Since 2009.</span><span style={{ fontWeight: 400 }}>.</span>.</div>
+                            <div className='home-quick-link-container'>
+                                <div className='home-quick-link-row'>
+                                    <Link to='/music' className='home-quick-link-button'>
+                                        <div className='home-quick-link-button-image' style={{ backgroundImage: `url(${quickLinkImageMusic})` }} />
+                                        <div className='home-quick-link-button-name' style={{ background: '#473d80' }}>Music Works</div>
+                                    </Link>
+                                    <Link to='/entertainment' className='home-quick-link-button'>
+                                        <div className='home-quick-link-button-image' style={{ backgroundImage: `url(${quickLinkImageEntertainment})` }} />
+                                        <div className='home-quick-link-button-name' style={{ background: '#c16a70' }}>Entertainments</div>
+                                    </Link>
+                                </div>
+                                <div className='home-quick-link-row'>
+                                    <Link to='/daigasso' className='home-quick-link-button'>
+                                        <div className='home-quick-link-button-image' style={{ backgroundImage: `url(${quickLinkImageDaigasso})` }} />
+                                        <div className='home-quick-link-button-name' style={{ background: '#d69739' }}>Daigasso! DX</div>
+                                    </Link>
+                                    <Link to='/gamevideo' className='home-quick-link-button'>
+                                        <div className='home-quick-link-button-image' style={{ backgroundImage: `url(${quickLinkImageGamevideo})` }} />
+                                        <div className='home-quick-link-button-name' style={{ background: '#8f3f39' }}>Game Videos</div>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Footer />
+                </Fragment>
+            }
         </div>
     );
 }
